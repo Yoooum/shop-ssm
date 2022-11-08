b6d317961bed6850f857ac9fd44b0e2a83deef7e
Asaka <Longzhu.Qianxun@qq.com>
2022-11-09 00:33:48 +0800

    Fix:Full .gitignore

--- src/main/java/com/prprv/ssm/pojo/User.java
+++ src/main/java/com/prprv/ssm/pojo/User.java
@@ -1,11 +1,15 @@
 package com.prprv.ssm.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author 未確認の庭師
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Integer uid;
     private String email;
